3fa6a110220636c0e1c2ae4e7c97e535fd9b5d6a
Doruk Karabulut <dorruk@github.com>
2018-11-27 21:16:34 +0300

    Added folders

---
+++ index.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+"https://www.gstatic.com/firebasejs/5.5.9/firebase.js"
+var config = {
+    apiKey: "AIzaSyDrX7YK1beCgxXA9Q6ftpbrdu8iQdod2ro",
+    authDomain: "testhoustongroup.firebaseapp.com",
+    databaseURL: "https://testhoustongroup.firebaseio.com",
+    projectId: "testhoustongroup",
+    storageBucket: "testhoustongroup.appspot.com",
+    messagingSenderId: "766386174489"
+  };
+  firebase.initializeApp(config);
+
+  
